7da8dfe036c179c89b874305fafe16bbeb57e379
Filipe Jose Maciel Ramalho <Filipe.Ramalho@SupTivits-MacBook-Pro.local>
2025-03-28 11:41:23 -0300

    Bumping version 1.24.0

--- Package.swift
+++ Package.swift
@@ -18,18 +18,18 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "STAIdentify",
-      url: "https://identify-pods.stoneage.com.br/STAIdentify/STAIdentify_1.23.3/STAIdentify.xcframework.zip",
-      checksum: "7790ab02c91eec77eb3607171ef4e1a4a9a8a1f6b0f5c425afdd0107e1fda964"
+      url: "https://identify-pods.stoneage.com.br/STAIdentify/STAIdentify_1.24.0/STAIdentify.xcframework.zip",
+      checksum: "f36b09dd2475fa667470dbbcaabbf3ae38ae91978f183a1a60828800319ec8bc"
     ),
     .binaryTarget(
       name: "IDLiveFaceCamera",
-      url: "https://identify-pods.stoneage.com.br/STAIdentify/STAIdentify_1.23.3/Libraries/iad-1_7_3/Frameworks/IDLiveFaceCamera.xcframework.zip",
-      checksum: "ebef0615ea8223d4033011e8bad4d1d040020aa14872fad04136ebbf2567149f"
+      url: "https://identify-pods.stoneage.com.br/STAIdentify/STAIdentify_1.24.0/Libraries/iad-1_7_3/Frameworks/IDLiveFaceCamera.xcframework.zip",
+      checksum: "ace84c87c5c1d0c4080b178587d20b5b7d3ceb78daa3c68cf8364950ade9d93f"
     ),
     .binaryTarget(
       name: "IDLiveFaceIAD",
-      url: "https://identify-pods.stoneage.com.br/STAIdentify/STAIdentify_1.23.3/Libraries/iad-1_7_3/Frameworks/IDLiveFaceIAD.xcframework.zip",
-      checksum: "c4bf1ac6782e9bf1f5623a3a011cbddc728d270604fde556e0d67ac8df3d2096"
+      url: "https://identify-pods.stoneage.com.br/STAIdentify/STAIdentify_1.24.0/Libraries/iad-1_7_3/Frameworks/IDLiveFaceIAD.xcframework.zip",
+      checksum: "e36eb8e049d0d3d03c4e6fc88edde1fdda02263dbff095fa72191c3a703508d5"
     ),
     .target(
       name: "STAIdentifySPM",
